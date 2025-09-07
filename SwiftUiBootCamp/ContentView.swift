68fe866e0c3f371adc1681641dc2c780368561ec
kajumac07 <kajumac13@gmail.com>
2025-09-07 16:51:01 +0530

    Initial Commit

---
+++ SwiftUiBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiBootCamp
+//
+//  Created by Atul Tiwari on 07/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiBootCamp/SwiftUiBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBootCampApp.swift
+//  SwiftUiBootCamp
+//
+//  Created by Atul Tiwari on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
