64b9a359bac56c21d1584467cf1ba754e3c9d70c
kajumac07 <kajumac13@gmail.com>
2025-09-12 19:23:11 +0530

    17. State property and color switch app

---
+++ SwiftUiBootCamp/StateBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  StateBootcamp.swift
+//  SwiftUiBootCamp
+//
+//  Created by Atul Tiwari on 12/09/25.
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var colorTitle : String = "Light Green"
+    
+    var body: some View {
+        
+        ZStack{
+            //background
+            backgroundColor.opacity(0.4)
+                .ignoresSafeArea()
+            
+            //content layer
+            VStack (spacing:20 ) {
+                Text(colorTitle)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    
+                Text("Count: 1")
+                    .font(.title)
+                    .fontWeight(.light)
+                
+                HStack (spacing:20){
+                    
+                    Button("Button 1"){
+                        self.backgroundColor = Color.red
+                        self.colorTitle = "Light Red"
+                    }
+                    
+                    Button("Button 2"){
+                        self.backgroundColor = Color.blue
+                        self.colorTitle = "Light Blue"
+                    }
+                }
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    StateBootcamp()
+}
