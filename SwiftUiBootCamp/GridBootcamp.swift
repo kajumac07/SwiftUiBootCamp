413fd98375621db59a76ce80c5d8a8ab66b709c5
kajumac07 <kajumac13@gmail.com>
2025-09-12 17:27:50 +0530

    14. LazyGridView

---
+++ SwiftUiBootCamp/GridBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  GridBootcamp.swift
+//  SwiftUiBootCamp
+//
+//  Created by Atul Tiwari on 12/09/25.
+//
+
+import SwiftUI
+
+struct GridBootcamp: View {
+
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+    ]
+
+    var body: some View {
+        ScrollView {
+            //            LazyVGrid(columns: columns) {
+            //                ForEach(0..<40) { index in
+            //                    Rectangle()
+            //                        .fill(.red)
+            //                        .frame(height: 150)
+            //                }
+            //            }
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 10,
+                      pinnedViews: [.sectionHeaders],
+                      content: {
+                Section {
+                    ForEach(0..<40) { index in
+                        Rectangle()
+                            .fill(.red)
+                            .frame(height: 150)
+                    }
+                } header: {
+                    Text("Header")
+                        .foregroundStyle(.white )
+                        .font(.title)
+                        .frame(maxWidth:.infinity, alignment: .leading)
+                        .background(.blue)
+                }
+                
+                Section {
+                    ForEach(0..<40) { index in
+                        Rectangle()
+                            .fill(.pink)
+                            .frame(height: 150)
+                    }
+                } header: {
+                    Text("Header 2")
+                        .foregroundStyle(.white )
+                        .font(.title)
+                        .frame(maxWidth:.infinity, alignment: .leading)
+                        .background(.blue)
+                }
+
+            })
+        }
+    }
+}
+
+#Preview {
+    GridBootcamp()
+}
